2ef7ff04c9f6bde3445def83cde7e965a9951b54
richard-melo <richard.meloneto@gmail.com>
2023-06-28 23:16:15 -0300

    Add class Orcamento

---
+++ Orcamento.java
@@ -0,0 +1,31 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+abstract class Orcamento {
+    private String Nome;
+    private String Descricao;
+    private LocalDate Data;
+    private double Valor;
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    public void setOrcamento(String nome, String descricao, LocalDate data, double valor){
+        this.Nome = nome;
+        this.Descricao = descricao;
+        this.Data = data;
+        this.Valor = valor;
+    }
+    public String getOrcamento (int mes, int ano){
+        String retorno = "";
+        if (Data.getMonthValue() == mes && Data.getYear() == ano) {
+            retorno = Nome + " | " + Descricao + " | " + formatter.format(Data) + " | " + Valor + "\n";
+        }
+        return retorno;
+    }
+    public double getValor(int mes, int ano){
+        double valor = 0;
+        if (Data.getMonthValue() == mes && Data.getYear() == ano){
+            valor = this.Valor;
+        }
+        return valor;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 1 - Adicionando classe Main e comentando códigos do programa antigo
+
+2 - Adicionando classe Orcamento para utilizar de base para gasto e ganho 
